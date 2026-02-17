7cc31cc10900a2170c2991c970bd377b4eab876a
SddIEG <artempalcev3@gmail.com>
2026-02-17 17:38:05 +0300

    fff
    Signed-off-by: SddIEG <artempalcev3@gmail.com>

--- src/IfElse1337.java
+++ src/IfElse1337.java
@@ -1,10 +1,23 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class IfElse1337 {
         public static void main(String[] args) {
-            boolean condition = false;
+            System.out.println("Введите ответ");
+            Scanner scanner = new Scanner(System.in);
+            // int x = scanner.nextInt();
+            String answer = scanner.nextLine();
+           // boolean condition = x < 0 || x > 10;
+            boolean condition = answer.equals("Да");
             if (condition) {
                 System.out.println("вы меня увидете, если condition = true");
             }  else {
-                System.out.println("вы меня увидете, если condition = false");
+                if (answer.equals("Нет")) {
+                    System.out.println("Вы ввели \"Нет\"");
+                } else {
+                    System.out.println("вы что - то не то ввели");
+                }
+
             }
             System.out.println("меня вы видите всегда");
         }
