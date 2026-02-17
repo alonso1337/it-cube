1d2b461d5034e22cc67cb0890352f84688a9a074
SddIEG <artempalcev3@gmail.com>
2026-02-17 17:09:37 +0300

    fff

---
+++ src/IfElse1337.java
@@ -0,0 +1,12 @@
+public class IfElse1337 {
+        public static void main(String[] args) {
+            boolean condition = false;
+            if (condition) {
+                System.out.println("вы меня увидете, если condition = true");
+            }  else {
+                System.out.println("вы меня увидете, если condition = false");
+            }
+            System.out.println("меня вы видите всегда");
+        }
+    }
+
